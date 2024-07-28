13217d913f2be42b2c2421c15b33aa9181f0259a
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-07-28 22:02:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 ip_version_priority = "ipv4"
 
 source_urls = [
+    "http://tv.850930.xyz/gather.m3u",
     "http://175.178.251.183:6689/channel.txt",
     "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
