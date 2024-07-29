f6172c99f03c46650aab8b6ea4f0b4754afff658
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-07-29 21:15:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,9 @@ source_urls = [
     "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
     "https://fm1077.serv00.net/SmartTV.m3u",
     "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
-    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
+    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt",
+    "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt",
+    "https://gitlab.com/tvkj/qxitv/-/raw/main/888.txt"
 ]
 
 url_blacklist = [
