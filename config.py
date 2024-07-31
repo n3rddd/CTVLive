a1a1e3b5695c0f9524b5019a87e02564ac2724f5
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-07-31 20:45:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ ip_version_priority = "ipv4"
 
 source_urls = [
     "http://tv.850930.xyz/kdsb.m3u", #ADDED BY LEM ON 29/07/2024
+    "http://tv.850930.xyz/kdsb2.m3u", #ADDED BY LEM ON 31/07/2024
     "http://tv.850930.xyz/gather.m3u", #ADDED BY LEM ON 29/07/2024
     "http://175.178.251.183:6689/channel.txt",
     "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
