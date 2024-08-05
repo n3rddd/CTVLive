5afa1a6ea9afcddb8565ad9893d22f7775d97b5d
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-08-05 10:00:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ source_urls = [
     "http://tv.850930.xyz/kdsb.m3u", #ADDED BY LEM ON 29/07/2024
     "http://tv.850930.xyz/kdsb2.m3u", #ADDED BY LEM ON 31/07/2024
     "http://tv.850930.xyz/gather.m3u", #ADDED BY LEM ON 29/07/2024
+    "http://tv.850930.xyz/543.m3u", #ADDED BY LEM ON 05/08/2024
+    "http://tv.850930.xyz/histar.m3u", #ADDED BY LEM ON 05/08/2024
     "http://175.178.251.183:6689/channel.txt",
     "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
@@ -32,7 +34,6 @@ source_urls = [
     "http://ttkx.live:55/lib/kx2024.txt", #ADDED BY LEM ON 29/07/2024
     "https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u", #ADDED BY LEM ON 29/07/2024
     "https://raw.githubusercontent.com/Love4vn/love4vn/main/Sport.m3u", #奥运 ON 29/07/2024
-    "https://raw.githubusercontent.com/hus888yu/app/main/13.txt", #奥运 ON 30/07/2024
     "https://gitlab.com/tvkj/qxitv/-/raw/main/888.txt" #ADDED BY LEM ON 29/07/2024
 ]
 
