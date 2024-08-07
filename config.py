8d936dd7d176cd633eaa5da84c0eee86cb5676d5
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-08-07 08:09:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ source_urls = [
     "http://tv.850930.xyz/gather.m3u", #ADDED BY LEM ON 29/07/2024
     "http://tv.850930.xyz/543.m3u", #ADDED BY LEM ON 05/08/2024
     "http://tv.850930.xyz/histar.m3u", #ADDED BY LEM ON 05/08/2024
-    "https://raw.githubusercontent.com/hus888yu/app/main/111.m3u", , #ADDED BY LEM ON 06/08/2024
+    "https://raw.githubusercontent.com/hus888yu/app/main/111.m3u", #ADDED BY LEM ON 06/08/2024
     "http://175.178.251.183:6689/channel.txt",
     "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
