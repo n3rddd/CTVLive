ea5d71cacccf4e381d6f9a155b8646c0bcac4f3a
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-08-08 11:14:12 +0800

    Update litemain.py

--- litemain.py
+++ litemain.py
@@ -170,7 +170,7 @@ def updateChannelUrlsM3U(channels, template_channels):
                                 new_url = f"{base_url}{url_suffix}"
 
                                 if base_url not in written_urls:
-                                    f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gitee.com/yuanzl77/TVBox-logo/raw/main/png/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
+                                    f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gitee.com/n3rddd/tvlogos/raw/main/logo/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
                                     f_m3u.write(new_url + "\n")
                                     f_txt.write(f"{channel_name},{new_url}\n")
                                     written_urls.add(base_url)
