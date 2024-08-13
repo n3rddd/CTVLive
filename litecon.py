92775ab7039dfd8a371475c5e6f6928e9af35fa6
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-08-13 21:20:22 +0800

    Update litecon.py

--- litecon.py
+++ litecon.py
@@ -6,7 +6,6 @@ source_urls = [
     "http://tv.850930.xyz/kdsb.m3u", #ADDED BY LEM ON 29/07/2024
     "http://tv.850930.xyz/kdsb2.m3u", #ADDED BY LEM ON 31/07/2024
     "http://tv.850930.xyz/gather.m3u", #ADDED BY LEM ON 29/07/2024
-    "http://tv.850930.xyz/543.m3u", #ADDED BY LEM ON 05/08/2024
     "https://raw.githubusercontent.com/hus888yu/app/main/111.m3u", #ADDED BY LEM ON 06/08/2024
     "https://raw.githubusercontent.com/hus888yu/app/main/543.m3u", #ADDED BY LEM ON 13/08/2024
     "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
