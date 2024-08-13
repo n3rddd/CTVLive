2561e3b3b31a895f0ec6014a108138eea0005fc9
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-08-13 21:11:51 +0800

    Update litecon.py

--- litecon.py
+++ litecon.py
@@ -8,6 +8,7 @@ source_urls = [
     "http://tv.850930.xyz/gather.m3u", #ADDED BY LEM ON 29/07/2024
     "http://tv.850930.xyz/543.m3u", #ADDED BY LEM ON 05/08/2024
     "https://raw.githubusercontent.com/hus888yu/app/main/111.m3u", #ADDED BY LEM ON 06/08/2024
+    "https://raw.githubusercontent.com/hus888yu/app/main/543.m3u", #ADDED BY LEM ON 13/08/2024
     "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
     "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt", #ADDED BY LEM ON 29/07/2024
     "http://ttkx.live:55/lib/kx2024.txt", #ADDED BY LEM ON 29/07/2024
