fd2efba0ccfc7fe37f8766d341ea4baf4327f6c0
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-08-26 19:51:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ source_urls = [
     "http://tv.850930.xyz/kdsb.m3u", #ADDED BY LEM ON 29/07/2024
     "http://tv.850930.xyz/kdsb2.m3u", #ADDED BY LEM ON 31/07/2024
     "http://tv.850930.xyz/gather.m3u", #ADDED BY LEM ON 29/07/2024
+    "https://tv543.cc/nb.txt", #ADDED BY LEM ON 26/08/2024
     "https://raw.githubusercontent.com/hus888yu/app/main/111.m3u", #ADDED BY LEM ON 06/08/2024
     "https://raw.githubusercontent.com/hus888yu/app/main/543.m3u", #ADDED BY LEM ON 13/08/2024
     "http://175.178.251.183:6689/channel.txt",
